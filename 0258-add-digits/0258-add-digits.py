ef8aceb12aada33e2e3d0731f7ee6b5bcbf1da66
Poornesh-lakshman <kollapureddypl@gmail.com>
2026-07-06 12:30:00 +0530

    Time: 5 ms (6.27%), Space: 12.4 MB (52.04%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -4,17 +4,7 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        # def ad(n):
-        #     a=0
-        #     while n>0:
-        #         r=n%10
-        #         a=a+r
-        #         n//=10
-        #     if a<=9:
-        #         return a
-        #     else:
-        #         return ad(a)
-        # return ad(num)
+        
 
         while num>=10:
             sum=0
