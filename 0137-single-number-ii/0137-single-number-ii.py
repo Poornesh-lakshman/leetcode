f3829d457a5be7c261dfb31284fc14417ac968af
Poornesh-lakshman <kollapureddypl@gmail.com>
2026-07-21 18:27:56 +0530

    Time: 0 ms (100%), Space: 20.9 MB (16.07%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,8 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-       one=0
-       two=0
-       for i in nums:
-            one=(one^i)&~two
-            two=(two^i)&~one
-       return one
+    #    one=0
+    #    two=0
+    #    for i in nums:
+    #         one=(one^i)&~two
+    #         two=(two^i)&~one
+    #    return one
+
+    
+        d={}
+        for i in nums:
+            d[i]=d.get(i,0)+1
+        for i,j in d.items():
+            if j==1:
+                return i
