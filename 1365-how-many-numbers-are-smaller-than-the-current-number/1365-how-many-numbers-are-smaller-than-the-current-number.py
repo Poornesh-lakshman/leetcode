809ab49ae94598df2da7788ee46a25451ef7de14
Poornesh-lakshman <kollapureddypl@gmail.com>
2026-07-27 12:39:19 +0530

    Time: 133 ms (45.71%), Space: 12.6 MB (8.34%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -5,11 +5,11 @@ class Solution(object):
         :rtype: List[int]
         """
         l=[]
-        
+        c=0
         for i in nums:
-            c=0
             for j in nums:
-                if i>j and i!=j:
+                if i>j :
                     c+=1
             l.append(c)
+            c=0
         return l
