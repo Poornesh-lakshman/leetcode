fc2d201cef8f0fcca64284a02a39b6d43e8e6719
Poornesh-lakshman <kollapureddypl@gmail.com>
2026-08-01 20:34:59 +0530

    Time: 3 ms (60.38%), Space: 19.4 MB (40.94%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -0,0 +1,6 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        d={}
+        for i in nums:
+            d[i]=d.get(i,0)+1
+        return sorted(nums, key=lambda x: (d[x], -x))
