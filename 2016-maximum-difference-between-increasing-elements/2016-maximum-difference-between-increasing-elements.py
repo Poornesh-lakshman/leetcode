cbb0762347c81b6a332a527190ee5de5ac08d3a3
Poornesh-lakshman <kollapureddypl@gmail.com>
2026-08-05 11:52:55 +0530

    Time: 0 ms (100%), Space: 12.5 MB (23.45%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
@@ -4,12 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ma=-1
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]<nums[j]:
-                    c=(nums[j]-nums[i])
-                    if c>ma:
-                        ma=c
-        return ma
+        # ma=-1
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]<nums[j]:
+        #             c=(nums[j]-nums[i])
+        #             if c>ma:
+        #                 ma=c
+        # return ma
+
+        a=nums[0]
+        r=-1
+        for i in range(1,len(nums)):
+            if nums[i]>a:
+                r=max(r,nums[i]-a)
+            else:
+                a=nums[i]
+        return r
       
