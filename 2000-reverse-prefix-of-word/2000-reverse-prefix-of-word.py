09deea90e5908333f814d3b0bf761e7694deac93
Poornesh-lakshman <kollapureddypl@gmail.com>
2026-08-19 22:58:23 +0530

    Time: 0 ms (100%), Space: 19.2 MB (57.08%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,11 +1,16 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        c=''
-        for i in range (len(word)):
-            c+=word[i]
-            j=i
-            if word[i]==ch:
-                break
+        # c=''
+        # for i in range (len(word)):
+        #     c+=word[i]
+        #     j=i
+        #     if word[i]==ch:
+        #         break
+        # if ch not in word:
+        #     return word
+        # return c[::-1]+word[j+1::]
         if ch not in word:
-            return word
-        return c[::-1]+word[j+1::]
+             return word
+        else:
+            a= word.index(ch)
+            return word[:a+1][::-1]+word[a+1:]
