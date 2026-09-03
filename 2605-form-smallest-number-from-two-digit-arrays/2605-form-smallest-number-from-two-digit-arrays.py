a6df9e4b74aded34467ee926f81df0d27245a23a
Poornesh-lakshman <kollapureddypl@gmail.com>
2026-09-03 14:57:08 +0530

    Time: 2 ms (12.64%), Space: 19.2 MB (62.07%) - LeetHub

---
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minNumber(self, nums1: List[int], nums2: List[int]) -> int:
+        c=[]
+        for i in nums1:
+            if i in nums2:
+                c.append(i)
+        if c:
+            return min(c)
+        a= min(nums1) 
+        b= min(nums2)
+        if a<b:
+            return int(str(a)+str(b))
+        return int(str(b)+str(a))
